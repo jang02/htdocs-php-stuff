43e4279d4e8cbba742bcef5c4240560f25bbcf7e
jan <jangarretsen01@gmail.com>
2020-03-20 17:00:33 +0100

    a
    a

--- Andreklussen revamp/pages/index.php
+++ Andreklussen revamp/pages/index.php
@@ -1,5 +1,23 @@
 <?php include("templates/header.html"); ?>
 
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner">
+            <div class="item active">
+                <img src="../imgs/bannerimg4.jpeg" alt="First Slide">
+                <h2 class="slider-text">TEST 1</h2>
+            </div>
 
+            <div class="item">
+                <img src="../imgs/bannerimg5.jpeg" alt="Second Slide">
+                <h2 class="slider-text">TEST 2</h2>
+            </div>
+
+            <div class="item">
+                <img src="../imgs/bannerimg3.jpeg" alt="Third Slide">
+                <h2 class="slider-text">TEST 3</h2>
+            </div>
+        </div>
+    </div>
 
 <?php include("templates/footer.html"); ?>
