c110a5afb37ea413205fff26fa0dcf322a7c8c97
jan <jangarretsen01@gmail.com>
2020-03-20 17:00:23 +0100

    update
    update

---
+++ Andreklussen revamp/pages/aboutus.php
@@ -0,0 +1,49 @@
+<?php include("templates/header.html"); ?>
+
+    <div>
+        <img src="../imgs/2.jpg" style="height: 500px;">
+    </div>
+
+    <div class="info">
+        <h1>Wie?</h1>
+        <p>André Garretsen, allround klusser.
+
+
+            Ik ben opgegroeid in het schildersvak, mijn vader had een schildersbedrijf in Doetinchem. Na mijn schooltijd
+            begon ik al jong met helpen in het bedrijf. Behangen, schilderen, spuiten, glas zetten het is mij met de
+            paplepel ingegeven. Toen mijn vader met het bedrijf stopte ben ik werkzaam geweest als stratenmaker,
+            stoffeerder en daarna jarenlang in de bouw. Met meer dan 30 jaar ervaring, kun je mij zeker een ervaren
+            allround klusser noemen!</p>
+    </div>
+
+    <div class="info">
+        <h1>Wat?</h1>
+        <p>Timmerwerk<br>
+
+            Aanbouw en verbouw<br>
+
+            Badkamer renovatie<br>
+
+            Keuken renovatie<br>
+
+            Installatie<br>
+
+            Metselen<br>
+
+            Straatwerk<br>
+
+            Montage<br>
+
+            Schilderwerk<br>
+
+            Renovatie<br>
+
+            Onderhoud</p>
+    </div>
+
+    <div class="info">
+        <h1>Waar?</h1>
+        <p>Nieuweweg 19,<br> Hardinxveld-Giessendam,<br> 3371 CJ</p>
+    </div>
+
+<?php include("templates/footer.html"); ?>
---
+++ Andreklussen revamp/pages/workinprogress.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Andreklussen</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="../css/andreklussen.css">
+</head>
+<body>
+<div class="wrapper">
+    <img id="wip-logo" src="../imgs/logo.gif">
+    <h1 id="wip-text">Hier verschijnt binnekort de website van Andreklussen.nl</h1>
+    <div class="push"></div>
+</div>
+<div class="footer">
+    <p>&copy; Andreklussen - 2020</p>
+</div>
+
+</body>
+</html>
