a4d017dfcb4904f656bbead980665a513e29942d
jan <jangarretsen01@gmail.com>
2020-03-24 14:41:44 +0100

    Andreklussen revamp
    made a gallery

---
+++ Andreklussen revamp/pages/gallery.php
@@ -0,0 +1,15 @@
+<?php include("templates/header.html"); ?>
+<h1 style="text-align: left;">Test Gallerij</h1>
+<div class="gallery">
+    <a href="../imgs/1.jpeg" data-lightbox="testgallerij" data-title="Test Gallerij"><img src="../imgs/1.jpeg"></a>
+    <a href="../imgs/2.jpeg" data-lightbox="testgallerij" data-title="Test Gallerij"><img src="../imgs/2.jpeg"></a>
+    <a href="../imgs/3.jpeg" data-lightbox="testgallerij" data-title="Test Gallerij"><img src="../imgs/3.jpeg"></a>
+    <a href="../imgs/4.jpeg" data-lightbox="testgallerij" data-title="Test Gallerij"><img src="../imgs/4.jpeg"></a>
+    <a href="../imgs/5.jpeg" data-lightbox="testgallerij" data-title="Test Gallerij"><img src="../imgs/5.jpeg"></a>
+    <a href="../imgs/6.jpeg" data-lightbox="testgallerij" data-title="Test Gallerij"><img src="../imgs/6.jpeg"></a>
+    <a href="../imgs/7.jpeg" data-lightbox="testgallerij" data-title="Test Gallerij"><img src="../imgs/7.jpeg"></a>
+    <a href="../imgs/8.jpeg" data-lightbox="testgallerij" data-title="Test Gallerij"><img src="../imgs/8.jpeg"></a>
+    <a href="../imgs/bannerimg4.jpeg" data-lightbox="testgallerij"><img src="../imgs/bannerimg4.jpeg"></a>
+</div>
+
+<?php include("templates/footer.html"); ?>
